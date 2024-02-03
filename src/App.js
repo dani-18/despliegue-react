c35a134454059ebc8db4dd9fe0be32d12b352ba0
dani-18 <danielalvarez4256@gmail.com>
2024-02-03 10:12:32 +0100

    Cambio en el titulo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola me llamo Dani
+          Aqui modifico
         </p>
         <a
           className="App-link"
